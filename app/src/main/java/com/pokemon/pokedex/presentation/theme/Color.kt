9875f23ba3f4506891f0fe26637eba88bbed90d4
Ramón <ramon.ma2017@gmail.com>
2025-06-05 23:28:47 -0600

    Update Theme & Colors

--- app/src/main/java/com/pokemon/pokedex/presentation/theme/Color.kt
+++ app/src/main/java/com/pokemon/pokedex/presentation/theme/Color.kt
@@ -2,10 +2,22 @@ package com.pokemon.pokedex.presentation.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val RedPokemonLight = Color(0xFFFF1C1C)
+val RedPokemonDark = Color(0xFFFF5C5C)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val YellowPokemonLight = Color(0xFFFFCB05)
+val YellowPokemonDark = Color(0xFFFFD54F)
+
+val BluePokemonLight = Color(0xFF3B4CCA)
+val BluePokemonDark = Color(0xFF6F78FF)
+
+val BackgroundLight = Color(0xFFF8F8F8)
+val SurfaceLight = Color(0xFFFFFFFF)
+val TextDark = Color(0xFF212121)
+
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+val TextLight = Color.White
+
+val ErrorLight = Color(0xFFD32F2F)
+val ErrorDark = Color(0xFFEF5350)
--- app/src/main/java/com/pokemon/pokedex/presentation/theme/Theme.kt
+++ app/src/main/java/com/pokemon/pokedex/presentation/theme/Theme.kt
@@ -9,29 +9,29 @@ import androidx.compose.ui.graphics.Color
 
 
 private val LightColorScheme = lightColorScheme(
-    primary = Color(0xFFFF1C1C),
-    secondary = Color(0xFFFFCB05),
-    tertiary = Color(0xFF3B4CCA),
-    background = Color(0xFFF8F8F8),
-    surface = Color(0xFFFFFFFF),
+    primary = RedPokemonLight,
+    secondary = YellowPokemonLight,
+    tertiary = BluePokemonLight,
+    background = BackgroundLight,
+    surface = SurfaceLight,
     onPrimary = Color.White,
     onSecondary = Color.Black,
-    onBackground = Color(0xFF212121),
-    onSurface = Color(0xFF212121),
-    error = Color(0xFFD32F2F)
+    onBackground = TextDark,
+    onSurface = TextDark,
+    error = ErrorLight
 )
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Color(0xFFFF5C5C),
-    secondary = Color(0xFFFFD54F),
-    tertiary = Color(0xFF6F78FF),
-    background = Color(0xFF121212),
-    surface = Color(0xFF1E1E1E),
+    primary = RedPokemonDark,
+    secondary = YellowPokemonDark,
+    tertiary = BluePokemonDark,
+    background = BackgroundDark,
+    surface = SurfaceDark,
     onPrimary = Color.Black,
     onSecondary = Color.Black,
-    onBackground = Color.White,
-    onSurface = Color.White,
-    error = Color(0xFFEF5350)
+    onBackground = TextLight,
+    onSurface = TextLight,
+    error = ErrorDark
 )
 
 @Composable
