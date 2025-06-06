60c7159a51a84cceb336174b716d4c182b09816f
Ramón <ramon.ma2017@gmail.com>
2025-06-06 12:24:02 -0600

    Add Vibration In Found Pokemon

--- app/src/main/java/com/pokemon/pokedex/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/pokemon/pokedex/presentation/ui/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.pokemon.pokedex.presentation.ui
 
 import android.Manifest
+import android.content.Context
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
+import android.os.VibratorManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -80,6 +85,15 @@ class MainActivity : ComponentActivity() {
 
                 LaunchedEffect(Unit) {
                     viewModel.showAlert.collect {
+                        val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                            val manager = context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+                            manager.defaultVibrator
+                        } else {
+                            @Suppress("DEPRECATION")
+                            context.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+                        }
+
+                        vibrator.vibrate(VibrationEffect.createOneShot(300, VibrationEffect.DEFAULT_AMPLITUDE))
                         scope.launch {
                             snackBarHostState.showSnackbar(getString(R.string.message_pokemon_found))
                         }
